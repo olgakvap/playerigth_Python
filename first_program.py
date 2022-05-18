1441a150005ea18851f54641e685b31364058d55
olgakvap <90151897+olgakvap@users.noreply.github.com>
2022-05-18 00:05:00 -0500

    first lessons

---
+++ first_program.py
@@ -0,0 +1,74 @@
+
+###to concat 2 strings
+# greeting = "Hello,"
+# name = "Olga"
+# full_greeting = f'{greeting} {name}'
+# print(full_greeting)
+# print(f'{full_greeting}!')
+
+### function
+# user_name = input("Please enter yiu name here: ")
+# print(f'Hello, {user_name}')
+
+### integers
+# int() str->int// int('1')==1
+# print(1+1)//2
+# print(int('1') + int('1'))//2
+
+# str()
+# a = 123
+# b = 45
+# print(a+b)//168
+# print(str(a) + str(b))//12345
+#or
+# user_input = input("Type a number: ")
+# print(int(user_input) + 5)
+
+### boolean //true or false
+# print(2==2)//true 2<=3, 'S'<'s'
+# print(2==3)//false 2!=3, 2>3, 'S'=='s'
+
+### if
+# a = 3
+# if a < 10:
+#     print("Code was ran")
+# else:
+#     print("The error!")
+#
+# print("The end of the program")
+
+### elif
+# a = 3
+# if a > 10:
+#     print("3 is greater then 10")
+# elif a > 9:
+#     print("3 is greater then 9")
+# elif a == 3:
+#     print("3 is equal to 3")
+# else:
+#     print("The error!")
+#
+# print("The end of the program")
+# if the several conditions are true, system will take the first one!!!!!!!!
+
+### boolean logic
+#and/or
+# a = 3
+# b = 4
+# c = 5
+# print(True and True)//true
+# print(a == 3 and b == 4)//true
+
+# print(True and False)//false
+# print((a == 3 or b == 44) and c == 55)//false (statement in braces is true, but last is false)
+
+# print(False and False)//false
+
+# print(True or False)//true or
+# print(f'True or False -> {True or False}')
+# print(a == 3 or b == 44 and c == 55)// true( one of statement is true)
+# print(a == 3 or (b == 44 and c == 55))//true
+
+###
+
+
